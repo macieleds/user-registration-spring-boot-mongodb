aa6568a1fb615607b3c77002ba2f6b9d120ca99d
Edison Maciel <maciel.eds@gmail.com>
2020-06-14 19:18:07 -0300

    Getting user by Id, implementing NotFoundExceptionHandler 404

--- src/main/java/com/edisonmaciel/userregistrationmongo/services/UserService.java
+++ src/main/java/com/edisonmaciel/userregistrationmongo/services/UserService.java
@@ -1,21 +1,28 @@
 package com.edisonmaciel.userregistrationmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.edisonmaciel.userregistrationmongo.domain.User;
 import com.edisonmaciel.userregistrationmongo.repository.UserRepository;
+import com.edisonmaciel.userregistrationmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado."));
 	}
 
 }
