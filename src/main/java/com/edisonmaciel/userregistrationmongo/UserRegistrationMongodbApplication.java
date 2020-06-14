84dc2418ba0da48a160e4eef7cefc5f155be32a9
Edison Maciel <maciel.eds@gmail.com>
2020-06-14 16:50:56 -0300

    First commit - Project created

---
+++ src/main/java/com/edisonmaciel/userregistrationmongo/UserRegistrationMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.edisonmaciel.userregistrationmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistrationMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edisonmaciel/userregistrationmongo/UserRegistrationMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edisonmaciel.userregistrationmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
