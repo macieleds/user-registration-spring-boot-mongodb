449b5cb7de5bfc0a9de6a24d16941de32d11c0cd
Edison Maciel <maciel.eds@gmail.com>
2020-06-14 21:38:32 -0300

    Creating entity Post with nested user

--- src/main/java/com/edisonmaciel/userregistrationmongo/config/Instantiation.java
+++ src/main/java/com/edisonmaciel/userregistrationmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.edisonmaciel.userregistrationmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.edisonmaciel.userregistrationmongo.domain.Post;
 import com.edisonmaciel.userregistrationmongo.domain.User;
+import com.edisonmaciel.userregistrationmongo.repository.PostRepository;
 import com.edisonmaciel.userregistrationmongo.repository.UserRepository;
 
 @Configuration
@@ -15,15 +19,29 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		userRepository.deleteAll();
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		userRepository.deleteAll();
+		postRepository.deleteAll();
+	
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post("1", sdf.parse("21/03/2018"), "Partiu Viagem!", "Vou vajar para São Paulo. Abraços.", maria);
+		Post post2 = new Post("1", sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje.", maria);
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 
 	}
 
--- src/main/java/com/edisonmaciel/userregistrationmongo/domain/Post.java
+++ src/main/java/com/edisonmaciel/userregistrationmongo/domain/Post.java
@@ -1,60 +1,76 @@
 package com.edisonmaciel.userregistrationmongo.domain;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection="user")
-public class User implements Serializable{
+@Document
+public class Post implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	private String id;
-	private String name;
-	private String email;
+	private Date date;
+	private String title;
+	private String body;
+	private User author;
 	
-	public User () {
+	public Post() {
 		
 	}
-
-	public User(String id, String name, String email) {
+	
+	public Post(String id, Date date, String title, String body, User author) {
 		super();
 		this.id = id;
-		this.name = name;
-		this.email = email;
+		this.date = date;
+		this.title = title;
+		this.body = body;
+		this.setAuthor(author);
 	}
-
-
+	
 	public String getId() {
 		return id;
 	}
-
 
 	public void setId(String id) {
 		this.id = id;
 	}
 
-
-	public String getName() {
-		return name;
+	public Date getDate() {
+		return date;
 	}
 
-
-	public void setName(String name) {
-		this.name = name;
+	public void setDate(Date date) {
+		this.date = date;
 	}
 
-
-	public String getEmail() {
-		return email;
+	public String getTitle() {
+		return title;
 	}
 
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
 	}
 	
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
 
 	@Override
 	public int hashCode() {
@@ -64,7 +80,6 @@ public class User implements Serializable{
 		return result;
 	}
 
-
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -73,7 +88,7 @@ public class User implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		User other = (User) obj;
+		Post other = (Post) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
