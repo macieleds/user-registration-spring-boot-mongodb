e3f5ece663be0f82a2aa8ffe80d07b94f0f39494
Edison Maciel <maciel.eds@gmail.com>
2020-06-14 22:57:21 -0300

    Adding comments to posts

--- src/main/java/com/edisonmaciel/userregistrationmongo/config/Instantiation.java
+++ src/main/java/com/edisonmaciel/userregistrationmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.edisonmaciel.userregistrationmongo.domain.Post;
 import com.edisonmaciel.userregistrationmongo.domain.User;
 import com.edisonmaciel.userregistrationmongo.dto.AuthorDTO;
+import com.edisonmaciel.userregistrationmongo.dto.CommentDTO;
 import com.edisonmaciel.userregistrationmongo.repository.PostRepository;
 import com.edisonmaciel.userregistrationmongo.repository.UserRepository;
 
@@ -32,19 +33,27 @@ public class Instantiation implements CommandLineRunner {
 		userRepository.deleteAll();
 		postRepository.deleteAll();
 	
-		User maria = new User(null, "Tinhoso Malandro", "chifroneto@gmail.com");
-		User alex = new User(null, "Alex Green", "alex@gmail.com");
-		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		User tinhoso = new User(null, "Tinhoso Malandro", "chifroneto@gmail.com");
+		User thigas = new User(null, "Thigas", "thigas@gmail.com");
+		User bob = new User(null, "Bob Smoke", "bobweed@gmail.com");
 		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		userRepository.saveAll(Arrays.asList(tinhoso, thigas, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!", "Vou vajar para São Paulo. Abraços.", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje.", new AuthorDTO(maria));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!", "Vou vajar para São Paulo. Abraços.", new AuthorDTO(tinhoso));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje.", new AuthorDTO(tinhoso));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(thigas));
+		CommentDTO c2 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(thigas));
+		
+		post1.getComment().addAll(Arrays.asList(c1, c2));
+		post2.getComment().addAll(Arrays.asList(c3));
+		
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
-		maria.getPosts().addAll(Arrays.asList(post1, post2));
-		userRepository.save(maria);
+		tinhoso.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(tinhoso);
 
 	}
 
--- src/main/java/com/edisonmaciel/userregistrationmongo/domain/Post.java
+++ src/main/java/com/edisonmaciel/userregistrationmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.edisonmaciel.userregistrationmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.edisonmaciel.userregistrationmongo.dto.AuthorDTO;
+import com.edisonmaciel.userregistrationmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -16,6 +19,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comment = new ArrayList<>();
 	
 	public Post() {
 		
@@ -72,6 +77,14 @@ public class Post implements Serializable{
 	}
 
 
+	public List<CommentDTO> getComment() {
+		return comment;
+	}
+
+	public void setComment(List<CommentDTO> comment) {
+		this.comment = comment;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -96,4 +109,5 @@ public class Post implements Serializable{
 			return false;
 		return true;
 	}
+
 }
